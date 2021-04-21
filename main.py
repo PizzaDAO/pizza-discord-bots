e8cd699bfecdc77898a01b272bea375a0c666e4d
David <ocanash1@illinois.edu>
2021-04-21 17:51:48 -0500

    Need to run webapp so container instance continues to run since we are using azure web apps

--- main.py
+++ main.py
@@ -5,15 +5,27 @@ import logging.config
 from dotenv import load_dotenv
 from os import path
 from discord.ext import commands
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
 
 
-def main():
-    load_dotenv()
+@app.route('/')
+def home():
+    return "I am alive! Molto Bene!!"
 
-    config_path = path.join(path.dirname(path.abspath(__file__)), 'config.ini')
-    logging.config.fileConfig(config_path, disable_existing_loggers=False)
-    logger = logging.getLogger(__name__)
 
+def run():
+    app.run(host='0.0.0.0', port=os.getenv('WEBSITES_PORT'))
+
+
+def web_app():
+    t = Thread(target=run)
+    t.start()
+
+
+def bot():
     intents = discord.Intents.default()
     intents.members = True
     bot = commands.Bot(command_prefix='$', intents=intents)
@@ -23,6 +35,17 @@ def main():
             bot.load_extension(f'cogs.{filename[:-3]}')
 
     bot.run(os.getenv('TOKEN'))
+
+
+def main():
+    load_dotenv()
+
+    config_path = path.join(path.dirname(path.abspath(__file__)), 'config.ini')
+    logging.config.fileConfig(config_path, disable_existing_loggers=False)
+    logger = logging.getLogger(__name__)
+
+    web_app()
+    bot()
 
 
 if __name__ == '__main__':
