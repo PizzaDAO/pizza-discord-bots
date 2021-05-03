3ddfa4cc580ee9169f64460df507969153b37b4f
Jkratz01 <53981021+Jkratz01@users.noreply.github.com>
2021-05-03 10:32:44 -0400

    Create help.py

---
+++ cogs/help.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+
+
+class Help(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+@bot.group
+async def help(ctx):
+    em = discord.Embed(title = "Help", description = "Use ?help <command> for extended information on a command. (Case Sensitive)", color = ctx.author.color)
+    em.add_field(name = 'Help Commands', value = 'mc,csv')
+
+    await ctx.send(embed = em)
+
+
+@help.command()
+async def mc(ctx):
+
+    em = discord.Embed(title = "mc", description = "Shows The Member Count Of The Pizza Dao Discord", color = ctx.author.color)
+    em.add_field(name = "**How To**", value = "$mc")
+    
+    await ctx.send(embed = em)
+
+
+@help.command()
+async def csv(ctx):
+
+    em = discord.Embed(title = "csv", description = "Plays A Song", color = ctx.author.color)
+    em.add_field(name = "**How To**", value = "$csv")
+    
+    await ctx.send(embed = em)
+
+
+def setup(bot):
+    bot.add_cog(Help(bot))
