8360b028ea1cc99c48ae71207fcfc1a8c8f086d1
Jkratz01 <53981021+Jkratz01@users.noreply.github.com>
2021-05-03 18:19:49 -0400

    Updating Main.py to work with help,py

--- main.py
+++ main.py
@@ -11,6 +11,21 @@ from threading import Thread
 app = Flask('')
 
 
+def bot():
+    intents = discord.Intents.default()
+    intents.members = True
+    bot = commands.Bot(command_prefix='$', intents=intents, help_command = None)
+
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py") and filename != "__init__.py":
+            bot.load_extension(f'cogs.{filename[:-3]}')
+
+
+    bot.run(os.getenv('TOKEN'))
+
+
+
+
 @app.route('/')
 def home():
     return "Super Pizza Trainer is alive and well! Molto Bene!"
@@ -24,26 +39,11 @@ def web_app():
     t = Thread(target=run)
     t.start()
 
-
-def bot():
-    intents = discord.Intents.default()
-    intents.members = True
-    bot = commands.Bot(command_prefix='$', intents=intents)
-
-    for filename in os.listdir("./cogs"):
-        if filename.endswith(".py") and filename != "__init__.py":
-            bot.load_extension(f'cogs.{filename[:-3]}')
-
-    bot.run(os.getenv('TOKEN'))
-
-
 def main():
     load_dotenv()
-
     config_path = path.join(path.dirname(path.abspath(__file__)), 'config.ini')
     logging.config.fileConfig(config_path, disable_existing_loggers=False)
     logger = logging.getLogger(__name__)
-
     web_app()
     bot()
 
