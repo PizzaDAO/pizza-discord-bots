c6be65817ccc34fd0d44eada1c358525fb57a45b
David <ocanash1@illinois.edu>
2021-05-24 21:56:37 -0500

    Add ability to collect videos. Also handle errors and clean up files

--- cogs/videos.py
+++ cogs/videos.py
@@ -9,33 +9,34 @@ from constants import DISCORD_PROJECTS
 logger = logging.getLogger(__name__)
 
 
-class Images(commands.Cog):
+class Videos(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=["images"])
-    async def download_all_images(self, ctx):
+    @commands.command(aliases=["videos"])
+    async def download_all_videos(self, ctx):
         """
-        Downloads all images in the channel, creates a zipfile containing all images, 
-            and returns the zipfile which can be downloaded via discord
+        Downloads all videos in the channel, creates a zipfile containing all videos, 
+            and returns the zip file which can be downloaded via discord
         """
-        image_types = ["png", "jpeg", "jpg"]
+        video_types = ["mp4", "webm", "mpg", "mp2", "mpeg", "mpe",
+                       "mpv", "ogg", "m4p", "m4v", "avi", "wmv", "qt", "flv", "swf"]
         project_dir = str(Path(__file__).parent.parent)
-        images_dir = project_dir + "/data/images/"
+        videos_dir = project_dir + "/data/videos/"
 
         count = 0
         async for message in ctx.channel.history():
             for attachment in message.attachments:
-                if any(attachment.filename.lower().endswith(img_type) for img_type in image_types):
+                if any(attachment.filename.lower().endswith(vid_type) for vid_type in video_types):
                     count += 1
                     file_name, file_type = attachment.filename.rsplit(".", 1)
                     # in case files have same name, use count to mark uniqueness
                     file_name += ("_" + str(count) + "." + file_type)
-                    file_path = images_dir + file_name
+                    file_path = videos_dir + file_name
                     await attachment.save(fp=file_path)
 
-        zip_file_path = project_dir + "/data/discord_images"
-        shutil.make_archive(zip_file_path, 'zip', images_dir)
+        zip_file_path = project_dir + "/data/discord_videos"
+        shutil.make_archive(zip_file_path, 'zip', videos_dir)
         zip_file_path += ".zip"
         try:
             await ctx.send(file=discord.File(zip_file_path))
@@ -43,9 +44,9 @@ class Images(commands.Cog):
             await ctx.channel.send("Oops, someone dropped the pizza on dis one! We ran into the following error: `{0}`".format(err) +
                                    "\nPlease reach out to the folks over at <#{0}> for help.".format(DISCORD_PROJECTS))
 
-        # remove all images and zip file
-        clean_up(images_dir, zip_file_path)
+        # remove all videos and zip file
+        clean_up(videos_dir, zip_file_path)
 
 
 def setup(bot):
-    bot.add_cog(Images(bot))
+    bot.add_cog(Videos(bot))
